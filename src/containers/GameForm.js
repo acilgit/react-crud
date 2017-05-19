02aa1f03c00a78a4d9d3ab641b8c5d062f65093f
xiuge <acil@163.com>
2017-05-19 18:02:12 +0800

    class 6 finish

--- src/containers/GameForm.js
+++ src/containers/GameForm.js
@@ -49,45 +49,47 @@ class GameForm extends React.Component {
         const isValid = Object.keys(errors).length === 0;
         if (isValid) {
             this.props.actions.setProps(types.gameForm, {loading: true});
-            this.props.actions.saveGame({title, cover}).then(data => console.log('then ',data));
+            this.props.actions.saveGame({title, cover}).then(data => console.log('then ', data));
         }
     };
 
     render() {
         let {title, cover, errors, loading, done} = this.props;
-        let Form = (<form className={classnames("ui form", {loading: loading})}
-                          onSubmit={this._onSubmit}>
-            <h1>Add new game</h1>
+        let Form = (
+            <form className={classnames("ui form", {loading: loading})}
+                  onSubmit={this._onSubmit}>
+                <h1>Add new game8</h1>
 
-            {!!errors.global && <div className="ui negative message"><p>{errors.global}</p></div>}
-            <div className={classnames("field", {error: !!errors.title})}>
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title"
-                       name="title"
-                       value={title}
-                       onChange={this._onChange}/>
-                <span>{errors.title}</span>
-            </div>
+                {!!errors.global && <div className="ui negative message"><p>{errors.global}</p></div>}
+                <div className={classnames("field", {error: !!errors.title})}>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title"
+                           name="title"
+                           value={title}
+                           onChange={this._onChange}/>
+                    <span>{errors.title}</span>
+                </div>
 
-            <div className={classnames("field", {error: !!errors.cover})}>
-                <label htmlFor="title">Cover URL</label>
-                <input type="text" id="cover"
-                       name="cover"
-                       value={cover}
-                       onChange={this._onChange}/>
-                <span>{errors.cover}</span>
-            </div>
+                <div className={classnames("field", {error: !!errors.cover})}>
+                    <label htmlFor="title">Cover URL</label>
+                    <input type="text" id="cover"
+                           name="cover"
+                           value={cover}
+                           onChange={this._onChange}/>
+                    <span>{errors.cover}</span>
+                </div>
 
-            <div className="filed">
-                {cover !== '' &&
-                <img src={cover} alt="cover" className="ui small bordered image"/>}
-            </div>
+                <div className="filed crop-img">
+                    {cover !== '3' &&
+                    <img style={ {height: "100%", width: "100%"}}
+                         src={"http://i.17173cdn.com/2fhnvk/YWxqaGBf/cms3/BdWHMUblpwmwCAo.jpg"} alt="cover"
+                         className="ui small cover bordered"/>}
+                </div>
 
-            <div className="field">
-                <button className="ui primary button">Save</button>
-            </div>
-
-        </form>);
+                <div className="field">
+                    <button className="ui primary button">Save</button>
+                </div>
+            </form>);
         return (
             <dii>
                 {done ? <Redirect to="/games"/> : Form}
